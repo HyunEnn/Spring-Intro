86fdc3242c25b3d07df0f499fba1c07c2711d150
HYUNJUN, KIM <denny10002@naver.com>
2022-11-26 17:44:06 +0900

    intro day 2

--- src/main/java/First/FirstSpring/SpringConfig.java
+++ src/main/java/First/FirstSpring/SpringConfig.java
@@ -1,24 +1,36 @@
 package First.FirstSpring;
 
+import First.FirstSpring.repository.JdbcMemberRepository;
 import First.FirstSpring.repository.MemberRepository;
 import First.FirstSpring.repository.MemoryMemberRepository;
 import First.FirstSpring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
-
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
+}
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
     // memberRepositoryf를 스프링 컨테이너에 주입시키고 MemberService 에 주입시킨 repository
     // 넣고 MemberService를 스프링 컨테이너에 주입시킨다.
 
-}
+
